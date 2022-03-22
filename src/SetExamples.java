a88c6c84a571bc5d67f04f67b7b52131bc795524
boris <boris@gratchev.com>
2022-03-22 16:49:44 +0100

    customizing sets

--- src/SetExamples.java
+++ src/SetExamples.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -17,5 +19,46 @@ public class SetExamples {
         System.out.println("Contains Gabi: " + names.contains("Gabi"));
         System.out.println("Remove Michael: " + names.remove("Michael"));
         System.out.println("Amount of Names: " + names.size());
+
+        System.out.println("\nsequential output: ");
+        for (String s : names){
+            System.out.println(s);
+        }
+
+        System.out.println("\nAdd Viktor: " + names.add("Viktor"));
+
+        System.out.println("\nsequential output: ");
+        Iterator<String> i = names.iterator();
+        while(i.hasNext()){
+            String name = i.next();
+            System.out.println(name);
+        }
+
+        System.out.println("\nWorking with custom Objects");
+        Set<FullName> fullNames = new TreeSet<>();
+
+        fullNames.add(new FullName("Boris", "Gratchev"));
+        fullNames.add(new FullName("Ivanka", "Trump"));
+        fullNames.add(new FullName("Ronald", "McDonald"));
+        fullNames.add(new FullName("Donald", "Trump"));
+
+        for(FullName currentName: fullNames){
+            System.out.println(currentName);
+        }
+
+
+        System.out.println("\nCustom Comparator:");
+        Set<FullName> namesBySignCount = new TreeSet<>(new SortFullNameBySignCount());
+
+        namesBySignCount.add(new FullName("Boris", "Gratchev"));
+        namesBySignCount.add(new FullName("Ivanka", "Trump"));
+        namesBySignCount.add(new FullName("Ronald", "McDonald"));
+        namesBySignCount.add(new FullName("Donald", "Trump"));
+
+        for(FullName currentName: namesBySignCount){
+            System.out.println(currentName);
+        }
+
+
     }
 }
---
+++ src/SortFullNameBySignCount.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class SortFullNameBySignCount implements Comparator<FullName> {
+    @Override
+    public int compare(FullName o1, FullName o2) {
+        int signCountO1 = o1.toString().length();
+        int signCountO2 = o2.toString().length();
+
+
+
+        return signCountO1 - signCountO2;
+    }
+}
