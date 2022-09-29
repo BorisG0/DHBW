c3c3bd1ac49cc0ac619106ba44e52d6bef946184
Boris Gratchev <boris@gratchev.com>
2022-09-29 17:12:58 +0200

    aufgabe 8 adv it

--- src/MyThread1.java
+++ src/MyThread1.java
@@ -1,14 +1,20 @@
 public class MyThread1 extends Thread{
     private int id;
+    static int runningThreats = 0;
     public MyThread1(int id){
         this.id = id;
     }
 
     public void run(){
+        runningThreats++;
         try{
             Thread.sleep((int) (Math.random() * 1000));
         }catch (Exception e) {}
         System.out.println("Hello World (ID = " + id + ")");
+        runningThreats--;
+        if(runningThreats == 0){
+            System.out.println("all threats stopped");
+        }
     }
 
     public static void main(String[] args) {
