d11abea2e7f2e3829f2feed9466383f454cbea99
Boris Gratchev <boris@gratchev.com>
2022-10-03 20:10:11 +0200

    aufgabe 9 adv it

--- src/MyThread1.java
+++ src/MyThread1.java
@@ -1,26 +1,32 @@
 public class MyThread1 extends Thread{
     private int id;
-    static int runningThreats = 0;
     public MyThread1(int id){
         this.id = id;
     }
 
     public void run(){
-        runningThreats++;
         try{
-            Thread.sleep((int) (Math.random() * 1000));
+            Thread.sleep((int) (Math.random() * 10000));
         }catch (Exception e) {}
         System.out.println("Hello World (ID = " + id + ")");
-        runningThreats--;
-        if(runningThreats == 0){
-            System.out.println("all threats stopped");
-        }
     }
 
     public static void main(String[] args) {
-        for(int i = 1; i < 10; i++) {
+        Thread[] threads = new MyThread1[9];
+        for(int i = 0; i < 9; i++) {
             Thread t = new MyThread1( i );
             t.start();
+            threads[i] = t;
         }
+
+        for(Thread t: threads){
+            try {
+                t.join();
+            }catch (Exception e){
+                System.out.println(e);
+            }
+        }
+
+        System.out.println("alle fertig");
     }
 }
