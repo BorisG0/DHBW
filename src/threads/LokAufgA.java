21cfe2dd16a7c34560becda9c0fb6746734dfa43
Boris Gratchev <boris@gratchev.com>
2022-10-15 10:44:55 +0200

    adv it testat 1 change

--- src/threads/LokAufgA.java
+++ src/threads/LokAufgA.java
@@ -5,34 +5,44 @@ import java.util.concurrent.Semaphore;
 public class LokAufgA {
     static Semaphore empty = new Semaphore(1, true);
 
-    static class Lok0 extends Thread{
+    static class Lok extends Thread{
+        int id;
+        int speed;
+        public Lok(int id, int speed){
+            this.id = id;
+            this.speed = speed;
+        }
+
         public void run(){
             try{
+                Thread.sleep(speed);
                 while(true){
                     //fahren
-                    enterLok0();
-                    Thread.sleep(1000);
-                    //Lok 0 im mittleren Teilstück
-                    exitLok0();
-                    Thread.sleep(2000);
+                    enter();
+
+                    //Lok im mittleren Teilstück
+                    Thread.sleep(speed);
+
+                    exit();
+                    Thread.sleep(speed * 2);
                 }
             }catch (Exception e){
                 System.out.println(e);
             }
         }
 
-        void enterLok0(){
+        void enter(){
             try{
-                System.out.println("Lok0 versucht auf mittleres Teilstueck zu fahren");
+                System.out.println("Lok" + id + " versucht auf mittleres Teilstueck zu fahren");
                 empty.acquire();
-                System.out.println("Lok0 befaehrt mittleres Teilstueck");
+                System.out.println("Lok" + id + " befaehrt mittleres Teilstueck");
             }catch (Exception e){
                 System.out.println(e);
             }
         }
-        void exitLok0(){
+        void exit(){
             try{
-                System.out.println("Lok0 verlaesst mittleres Teilstueck");
+                System.out.println("Lok" + id + " verlaesst mittleres Teilstueck");
                 empty.release();
             }catch (Exception e){
                 System.out.println(e);
@@ -40,44 +50,10 @@ public class LokAufgA {
         }
     }
 
-    static class Lok1 extends Thread{
-        public void run(){
-            try{
-                while(true){
-                    //fahren
-                    enterLok1();
-                    Thread.sleep(1500);
-                    //Lok 1 im mittleren Teilstück
-                    exitLok1();
-                    Thread.sleep(2500);
-                }
-            }catch (Exception e){
-                System.out.println(e);
-            }
-        }
-
-        void enterLok1(){
-            try{
-                System.out.println("Lok1 versucht auf mittleres Teilstueck zu fahren");
-                empty.acquire();
-                System.out.println("Lok1 befaehrt mittleres Teilstueck");
-            }catch (Exception e){
-                System.out.println(e);
-            }
-        }
-        void exitLok1(){
-            try{
-                System.out.println("Lok1 verlaesst mittleres Teilstueck");
-                empty.release();
-            }catch (Exception e){
-                System.out.println(e);
-            }
-        }
-    }
 
 
     public static void main(String[] args) {
-        new Lok0().start();
-        new Lok1().start();
+        new Lok(0, 1000).start();
+        new Lok(1, 1500).start();
     }
 }
