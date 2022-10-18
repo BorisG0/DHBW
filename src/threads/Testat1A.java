f51fc286c66b85b850d0a95ce1a8856a33c9d1a3
Boris Gratchev <boris@gratchev.com>
2022-10-18 15:50:51 +0200

    adv it testat1a

---
+++ src/threads/Testat1A.java
@@ -0,0 +1,85 @@
+package threads;
+
+import java.util.concurrent.Semaphore;
+
+public class Testat1A {
+    static Semaphore empty = new Semaphore(1, true);
+    static Semaphore full = new Semaphore(0, true);
+    static class Lok0 implements Runnable{
+
+        @Override
+        public void run() {
+            try{
+                Thread.sleep(1000);
+                while(true){
+                    enterLok0();
+                    System.out.println("Lok0 auf mittlerer Schiene");
+                    Thread.sleep(1000); //Zeit um mittlere Schiene vollständig zu befahren
+                    exitLok0();
+                    Thread.sleep(5000); //Zeit um den Rest des Kreis zu befahren
+                }
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        }
+
+        void enterLok0(){
+            try{
+                System.out.println("Lok0 vor mittlerer Schiene");
+                empty.acquire();
+            }catch (Exception e ){
+                System.out.println(e);
+            }
+        }
+
+        void exitLok0(){
+            try{
+                System.out.println("Lok0 verlaesst mittlere Schiene");
+                full.release();
+            }catch (Exception e ){
+                System.out.println(e);
+            }
+        }
+    }
+
+    static class Lok1 implements Runnable{
+
+        @Override
+        public void run() {
+            try{
+                while(true){
+                    enterLok1();
+                    System.out.println("Lok1 auf mittlerer Schiene");
+                    Thread.sleep(500); //Zeit um mittlere Schiene vollständig zu befahren
+                    exitLok1();
+                    Thread.sleep(1500); //Zeit um den Rest des Kreis zu befahren
+                }
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        }
+
+        void enterLok1(){
+            try{
+                System.out.println("Lok1 vor mittlerer Schiene");
+                full.acquire();
+            }catch (Exception e ){
+                System.out.println(e);
+            }
+        }
+
+        void exitLok1(){
+            try{
+                System.out.println("Lok1 verlaesst mittlere Schiene");
+                empty.release();
+            }catch (Exception e ){
+                System.out.println(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new Lok0()).start();
+        new Thread(new Lok1()).start();
+    }
+}
