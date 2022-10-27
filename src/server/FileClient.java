0ba80f284b39f5c1b41d858b9e0aa2929061c36e
Boris Gratchev <boris@gratchev.com>
2022-10-27 11:12:07 +0200

    adv it 13 b

--- src/server/FileClient.java
+++ src/server/FileClient.java
@@ -4,26 +4,30 @@ import java.io.IOException;
 import java.net.*;
 import java.util.Scanner;
 
-public class ChatClient {
+public class FileClient {
     public static void main(String[] args) {
         try {
             DatagramSocket socket = new DatagramSocket();
-
-            byte[] data = "first message".getBytes();
-
+            byte[] data;
             InetAddress address = InetAddress.getByName("localhost");
-            DatagramPacket packet = new DatagramPacket(data, data.length, address, ChatServer.serverPort);
-
-            //socket.send(packet);
+            DatagramPacket packet;
 
             Scanner scanner = new Scanner(System.in);
-            String message = "";
+            String message;
 
             while (true){
                 message = scanner.nextLine();
                 data = message.getBytes();
-                packet = new DatagramPacket(data, data.length, address, ChatServer.serverPort);
+                packet = new DatagramPacket(data, data.length, address, 4999);
                 socket.send(packet);
+
+                if(message.split(" ")[0].equals("read")){
+                    DatagramPacket receivePacket = new DatagramPacket(new byte[1024], 1024);
+                    socket.receive(receivePacket);
+
+                    String requestedLine = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                    System.out.println(requestedLine);
+                }
             }
         } catch (SocketException e) {
             e.printStackTrace();
--- src/server/FileServer.java
+++ src/server/FileServer.java
@@ -1,35 +1,87 @@
 package server;
 
 import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.ArrayList;
 
 public class FileServer {
+    private static String relativePath = new File("").getAbsolutePath().concat("/textFiles/");
+    public final static int serverPort = 4999;
+
     public static void main(String[] args) {
-        System.out.println(getLine("myText.txt", 3));
+        try {
+            System.out.println("server started");
+            DatagramSocket socket = new DatagramSocket(serverPort);
+
+            while(true){
+                DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+                socket.receive(packet);
+
+                String message = new String(packet.getData(), 0, packet.getLength());
+                String[] parts = message.split(" ");
+
+                if(parts[0].equals("read")){
+                    String requestedLineText = getLine(parts[1], Integer.parseInt(parts[2]));
+
+                    byte[] data = requestedLineText.getBytes();
+                    DatagramPacket returnPacket = new DatagramPacket(data, data.length, packet.getAddress(), packet.getPort());
+                    socket.send(returnPacket);
+                }else if(parts[0].equals("write")){
+                    writeLine(parts[1], Integer.parseInt(parts[2]), parts[3]);
+                }
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }catch (IOException e){
+            System.err.println(e);
+        }
     }
 
     private static String getLine(String fileName, int lineNumber){
         String returnLine = "";
-        String filePath = new File("").getAbsolutePath();
         BufferedReader fileIn = null;
+
         try {
-            fileIn = new BufferedReader(new FileReader(filePath.concat("/src/server/" + fileName)));
+            fileIn = new BufferedReader(new FileReader(relativePath + fileName));
         } catch (FileNotFoundException e) {
             System.out.println(e);
+            return "";
         }
 
-
         try {
-            String line = null;
-            line = fileIn.readLine();
-            while(line != null){
-                System.out.println(line);
-                line = fileIn.readLine();
+            for(int i = 0; i < lineNumber - 1; i++){
+                fileIn.readLine();
             }
-
+            returnLine = fileIn.readLine();
         } catch (IOException e) {
             System.out.println(e);
         }
 
         return returnLine;
+    }
+
+    private static void writeLine(String fileName, int lineNumber, String newLine){
+        try {
+            ArrayList<String> allLines = new ArrayList<>();
+            BufferedReader fileIn = new BufferedReader(new FileReader(relativePath + fileName));
+            String line = fileIn.readLine();
+            while(line != null){
+                allLines.add(line);
+                line = fileIn.readLine();
+            }
+            allLines.add(lineNumber, newLine);
+
+            PrintWriter fileOut = new PrintWriter(new FileWriter(relativePath + fileName));
+
+            for (String s: allLines){
+                fileOut.println(s);
+            }
+            fileOut.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
